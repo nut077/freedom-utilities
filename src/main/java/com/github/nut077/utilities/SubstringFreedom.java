0c547c90f35485bb13da4a5898f11548abad09a6
nuttapon.t <nuttapon.tavee@gmail.com>
2018-05-10 19:20:16 +0700

    Initial commit

---
+++ src/main/java/com/github/nut077/utilities/SubstringFreedom.java
@@ -0,0 +1,34 @@
+package com.github.nut077.utilities;
+
+public class SubstringFreedom {
+    private CheckFreedom check = new CheckFreedom();
+
+    public SubstringFreedom() {
+
+    }
+
+    public String substring(String str, int first) {
+        String val = "";
+        if (isTrueCondition(str, first)) {
+            val = str.substring(first);
+        }
+        return val;
+    }
+
+    public String substring(String str, int first, int last) {
+        String val = "";
+        if (isTrueCondition(str, last) && first < last && first != -1) {
+            val = str.substring(first, last);
+        }
+        return val;
+    }
+
+    private boolean isTrueCondition(String str, int length) {
+        if (check.isNotBlank(str) && length != -1) {
+            if (str.length() >= length) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
